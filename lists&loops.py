bbe1a4764b12cbeb99bc0c13b86e242d48539208
Zayd Antony <zaydantony123@gmail.com>
2025-10-16 17:03:48 +0300

    Day 2 list & loops

---
+++ lists&loops.py
@@ -0,0 +1,8 @@
+# Exercise Build a program that finds the largest and smallest numbers in a list.
+
+numbers = [1,2,8,12,13,14,15,28,56];
+
+for num in numbers:
+    print(max(numbers));
+    print(min(numbers));
+    break;
